67ff5d4682772922f49bfacdac270a4ff164b26c
jfavarin <favarin.jonas@gmail.com>
2025-02-22 11:28:55 -0300

    segundo exercicio

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,17 @@
+import math
+
+# exercicio 1 
+
 n1 = int(input("Insira um numero inteiro: "))
 n2 = int(input("Insira um outro numero inteiro: "))
 
 res = n1 // n2
 
 print(res)
+
+# exercicio 2
+
+r_circ = float(input("Enter de circle radius: "))
+a_circ = math.pi * r_circ ** 2
+
+print("{:.2f}".format(a_circ))
