6ce4ae826d098f3c1254f5e427aee89c2a0dec59
jfavarin <favarin.jonas@gmail.com>
2025-02-22 11:30:31 -0300

    segundo exercicio ajustado

--- exercicios.py
+++ exercicios.py
@@ -14,4 +14,5 @@ print(res)
 r_circ = float(input("Enter de circle radius: "))
 a_circ = math.pi * r_circ ** 2
 
-print("{:.2f}".format(a_circ))
+# print("{:.2f}".format(a_circ)) # old method
+print(f"{a_circ:.2f}")
